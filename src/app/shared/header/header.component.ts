202c0c033c7a1a891bf919d714f5d9a349646afc
Jose Luis Pintado Vasquez <i201716389@cibertec.edu.pe>
2021-08-19 00:34:50 -0500

    Creación de Contador de fin de ciclo

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import *  as countdown from 'countdown';
+interface Time{
+  days:number;
+  hours:number;
+  minutes:number;
+  seconds:number;
+}
 
 @Component({
   selector: 'app-header',
@@ -6,10 +13,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  time:Time=null;
+  timerId:number=null;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit( ): void {
+    let date=new Date("2021-08-21");
+   countdown(date,(ts)=>{
+     this.time=ts;
+   }, countdown.DAYS |countdown.HOURS | countdown.MINUTES | countdown.SECONDS);
+  }
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    if(this.timerId){
+clearInterval(this.timerId);
+    }
   }
 
 }
