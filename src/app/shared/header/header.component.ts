35a3b08a13b7451e5669578824d98d3432aa5b54
Jose Luis Pintado Vasquez <i201716389@cibertec.edu.pe>
2021-10-02 12:10:47 -0500

    Cambios en home

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import *  as countdown from 'countdown';
 import Swal from 'sweetalert2';
 
-interface Time{
-  days:number;
-  hours:number;
-  minutes:number;
-  seconds:number;
+interface Time {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
 }
 
 @Component({
@@ -15,28 +15,28 @@ interface Time{
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  time:Time=null;
-  timerId:number=null;
+  time: Time = null;
+  timerId: number = null;
   constructor() { }
-  alertInscrpcion(){
+  alertInscrpcion() {
     Swal.fire({
       text: "Sé que estas entusiasmado, tendrás más información en Diciembre ",
       icon: "info",
       timer: 5000,
       timerProgressBar: true
-  });
+    });
   }
-  ngOnInit( ): void {
-    let date=new Date("2021-09-21 18:00:00");
-   countdown(date,(ts)=>{
-     this.time=ts;
-   }, countdown.DAYS |countdown.HOURS | countdown.MINUTES | countdown.SECONDS);
+  ngOnInit(): void {
+    let date = new Date("2021-10-05 18:00:00");
+    countdown(date, (ts) => {
+      this.time = ts;
+    }, countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS);
   }
   ngOnDestroy(): void {
     //Called once, before the instance is destroyed.
     //Add 'implements OnDestroy' to the class.
-    if(this.timerId){
-clearInterval(this.timerId);
+    if (this.timerId) {
+      clearInterval(this.timerId);
     }
   }
 
