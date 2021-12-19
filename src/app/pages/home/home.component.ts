13b0abb2d511fcf3436243c58e9c256d4ed84b24
Jose Luis Pintado Vasquez <i201716389@cibertec.edu.pe>
2021-12-19 08:56:43 -0500

    Creando downCount

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,19 +2,38 @@ import { Component, OnInit } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
 import Swal from 'sweetalert2';
 import { InfoPaginaService } from '../../services/info-pagina.service';
-
+import *  as countdown from 'countdown';
+interface Time {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  time: Time = null;
+  timerId: number = null;
+  constructor(public _eventos: InfoPaginaService) {
 
-  constructor(public _eventos:InfoPaginaService) {
-    
-   }
+  }
 
   ngOnInit(): void {
+    let date = new Date("2021-12-31");
+    countdown(date, (ts) => {
+      this.time = ts;
+    }, countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS);
+
+  }
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    if (this.timerId) {
+      clearInterval(this.timerId);
+    }
   }
   customOptions: OwlOptions = {
     loop: true,
@@ -41,12 +60,12 @@ export class HomeComponent implements OnInit {
     nav: true
   }
 
-  alertInscrpcion(){
+  alertInscrpcion() {
     Swal.fire({
       text: "El grupo 1 inició el 13 dic. 2021. Sin embargo, el grupo 2 está disponible. ",
       icon: "info",
       timer: 5000,
       timerProgressBar: true
-  });
+    });
   }
 }
